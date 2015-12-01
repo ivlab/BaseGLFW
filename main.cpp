dac694c7da55c90baa8a1ca734ea0f273da61b12
Dan Orban <dtorban@umn.edu>
2015-12-01 11:33:04 -0600

    Add mouse button.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <vector>
+#include "event/VREvent.h"
+#include <net/VRNetClient.h>
+
 static void error_callback(int error, const char* description)
 {
     fputs(description, stderr);
@@ -13,8 +17,35 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
+std::vector<VREvent> events;
+
+static void buttoncb(GLFWwindow* window, int button, int action, int mods)
+{
+    VRDataIndex di;
+
+	(void)window;
+	(void)mods;
+	if (button == GLFW_MOUSE_BUTTON_LEFT ) {
+		if (action == GLFW_PRESS) {
+		    VREvent e("Mouse_Down", di);
+		    events.push_back(e);
+			//input.mbut |= MG_MOUSE_PRESSED;
+		}
+		if (action == GLFW_RELEASE) {
+		    VREvent e("Mouse_Up", di);
+		    events.push_back(e);
+			//input.mbut |= MG_MOUSE_RELEASED;
+		}
+	}
+}
+
+
 int main(void)
 {
+	//VRNetClient client("134.84.121.29", "3490");
+	VRNetClient client("localhost", "19999");
+
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
@@ -28,8 +59,10 @@ int main(void)
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
     glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, buttoncb);
     while (!glfwWindowShouldClose(window))
     {
+        client.synchronizeInputEventsAcrossAllNodes(events);
         float ratio;
         int width, height;
         glfwGetFramebufferSize(window, &width, &height);
@@ -50,6 +83,8 @@ int main(void)
         glColor3f(0.f, 0.f, 1.f);
         glVertex3f(0.f, 0.6f, 0.f);
         glEnd();
+        events.clear();
+        client.synchronizeSwapBuffersAcrossAllNodes();
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
